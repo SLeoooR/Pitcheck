caf48eb88bb90b8f8df9d03324ddfb90daf266a5
SLeoooR <sreganit@gbox.adnu.edu.ph>
2022-03-06 21:27:43 +0800

    Whoops forgot to add these

--- lib/pitches_tab/pitch_tile.dart
+++ lib/pitches_tab/pitch_tile.dart
@@ -19,9 +19,22 @@ class PitchTile extends StatefulWidget {
 class _PitchTileState extends State<PitchTile> {
   @override
   Widget build(BuildContext context) {
+    Widget? leadingListTile =
+      widget.pitch.icon == 'pending' ? const Icon(Icons.pending_outlined, color: Colors.black,) :
+      widget.pitch.icon == 'dissatisfied' ? const Icon(Icons.sentiment_dissatisfied_outlined, color: Colors.black,) :
+      widget.pitch.icon == 'neutral' ? const Icon(Icons.sentiment_neutral_outlined, color: Colors.black,) :
+      widget.pitch.icon == 'satisfied' ? const Icon(Icons.sentiment_satisfied_outlined, color: Colors.black,) : null;
+
+    Color? cardColor =
+      widget.pitch.icon == 'pending' ? Colors.lightBlueAccent :
+      widget.pitch.icon == 'dissatisfied' ? Colors.redAccent :
+      widget.pitch.icon == 'neutral' ? Colors.yellowAccent :
+      widget.pitch.icon == 'satisfied' ? Colors.lightGreen: null;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Card(
+        color: cardColor,
         elevation: 5,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
@@ -29,22 +42,21 @@ class _PitchTileState extends State<PitchTile> {
         ),
         child: InkWell(
           onTap: () {
-            print(widget.prefsKey);
             Navigator.push(context, MaterialPageRoute(builder: (context) => PitchDetails(pitch: widget.pitch, prefsKey: widget.prefsKey)));
           },
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: <Widget>[
-                Text(DateTime.now().toString(), style: const TextStyle(color: Colors.grey)),
+                Text(DateTime.now().toString()),
                 const Divider(
-                  color: Colors.grey,
+                  color: Colors.black,
                   thickness: .5,
                 ),
                 ListTile(
-                  leading: widget.pitch.icon == 'pending' ? const Icon(Icons.pending_outlined) : null,
-                  title: Text(widget.pitch.title),
-                  subtitle: Text(widget.pitch.company)
+                  leading: leadingListTile,
+                  title: Text(widget.pitch.title, style: const TextStyle(color: Colors.black),),
+                  subtitle: Text(widget.pitch.company, style: const TextStyle(color: Colors.black),)
                 ),
               ],
             ),
--- lib/profile_tab/profile.dart
+++ lib/profile_tab/profile.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:pitcheck/intro_screens/load_screen.dart';
+import 'package:pitcheck/main.dart';
 import 'package:pitcheck/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -23,23 +24,35 @@ class _ProfileState extends State<Profile> {
 
           User user = User.fromJson(json.decode(prefs?.getString('userData') ?? ""));
 
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Lottie.asset('assets/avatar.json', width: 200, height: 200),
-                Text.rich(
-                  TextSpan(
+          return SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: deviceWidth(context) * .1, vertical: deviceHeight(context) * .1),
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  //set border radius more than 50% of height and width to make circle
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const TextSpan(text:'I am ', style: TextStyle(fontSize: 25, )),
-                      TextSpan(text: user.name , style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.green)),
+                      Lottie.asset('assets/avatar.json', width: 200, height: 200),
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            const TextSpan(text:'I am ', style: TextStyle(fontSize: 25, )),
+                            TextSpan(text: user.name , style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.green)),
+                          ],
+                        ),
+                      ),
+                      Text('An excellent ${user.occupation}', style: const TextStyle(fontSize: 20),),
+                      const SizedBox(height: 40,),
+                      Text('Total Pitches: ${user.totalPitches}', style: const TextStyle(fontSize: 20),),
                     ],
                   ),
                 ),
-                Text('"${user.occupation}"', style: const TextStyle(fontSize: 20),),
-                const SizedBox(height: 40,),
-                Text('Total Pitches: ${user.totalPitches}', style: const TextStyle(fontSize: 20),),
-              ],
+              ),
             ),
           );
         } else {
